ba89f26f546673bd8548845f5d3583ad5dfc4113
Yulian Popovych <popovuch02@gmail.com>
2021-10-03 18:44:22 +0200

    Commit after Rebase

--- src/Main.java
+++ src/Main.java
@@ -6,7 +6,5 @@ public class Main {
         System.out.println("First Alpha");
         System.out.println("Second Alpha");
         System.out.println("Third Alpha");
-        System.out.println("First Bravo");
-        System.out.println("Second Bravo");
     }
 }
